e8615374be8bec09bcff226aa85e67121fa6713a
west130 <felipeeestrada9803@gmail.com>
2019-01-31 01:23:48 -0500

    nueva2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Su madre hoalslslslslalala
+                    holaaa
                 </div>
 
                 <div class="links">
